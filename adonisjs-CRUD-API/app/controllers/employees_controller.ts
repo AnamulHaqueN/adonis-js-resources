42a914cf343a6972af25885cb9400b132c83e563
AnamulHaqueN <rafaielanam@gmail.com>
2025-12-04 18:27:27 +0600

    CRUD operation is completed

--- adonisjs-CRUD-API/app/controllers/employees_controller.ts
+++ adonisjs-CRUD-API/app/controllers/employees_controller.ts
@@ -1,10 +1,15 @@
+import Employee from '#models/employee'
+import { EmployeeUpdateValidator, EmployeeValidator } from '#validators/employee'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class EmployeesController {
   /**
    * Display a list of resource
    */
-  async index({}: HttpContext) {}
+  async index({ response }: HttpContext) {
+    const employees = await Employee.all()
+    return response.status(200).json({ data: employees })
+  }
 
   /**
    * Display form to create a new record
@@ -14,25 +19,78 @@ export default class EmployeesController {
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) {}
+  async store({ request, response }: HttpContext) {
+    try {
+      const data = await request.validateUsing(EmployeeValidator)
+
+      const employee = await Employee.create({
+        name: data.name,
+        email: data.email,
+        mobile: request.input('mobile'),
+        dob: request.input('dob'),
+        doj: request.input('doj'),
+        address: request.input('address'),
+      })
+
+      return response
+        .status(200)
+        .json({ messages: 'Employee created successfully', data: employee })
+    } catch (error) {
+      return response.badRequest(error.message)
+    }
+  }
 
   /**
    * Show individual record
    */
-  async show({ params }: HttpContext) {}
+  async show({ params, response }: HttpContext) {
+    const employee = await Employee.find(params.id)
+
+    return response.status(200).json({ data: employee })
+  }
 
   /**
    * Edit individual record
    */
-  async edit({ params }: HttpContext) {}
+  async edit({}: HttpContext) {}
 
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request, response }: HttpContext) {
+    const employee = await Employee.findOrFail(params.id)
+
+    try {
+      const data = await request.validateUsing(EmployeeUpdateValidator)
+
+      employee.merge(data)
+
+      await employee.save()
+      return response
+        .status(200)
+        .json({ messages: 'Employee updated successfully', data: employee })
+    } catch (error) {
+      return response.badRequest(error.message)
+    }
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params, response }: HttpContext) {
+    try {
+      const employee = await Employee.find(params.id)
+      if (!employee) {
+        return response.status(404).json({ type: 'error', message: 'Employee is not Found !' })
+      }
+      await employee.delete()
+
+      return {
+        type: 'success',
+        message: 'Employee deleted successfully',
+      }
+    } catch (error) {
+      return response.badRequest(error.message)
+    }
+  }
 }

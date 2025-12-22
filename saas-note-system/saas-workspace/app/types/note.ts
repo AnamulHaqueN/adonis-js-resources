a1f931d65f50476b054ab2d161c4108a3d867e37
AnamulHaqueN <rafaielanam@gmail.com>
2025-12-22 11:16:58 +0600

    fix: issues and change backend folder name

--- saas-note-system/saas-workspace/app/types/note.ts
+++
@@ -1,2 +0,0 @@
-// app/types/note.ts
-export type NoteType = 'public' | 'private'
--- saas-note-system/saas-workspace/app/validators/loginvalidator.ts
+++
@@ -1,8 +0,0 @@
-import vine from '@vinejs/vine'
-
-export const LoginValidator = vine.compile(
-  vine.object({
-    email: vine.string().email().toLowerCase(),
-    password: vine.string(),
-  })
-)
